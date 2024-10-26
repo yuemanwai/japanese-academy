743b3ef5e14fd1cf776315c425fd93e6b1926428
Ashley Yue <143982071+yuemanwai@users.noreply.github.com>
2024-10-26 12:21:11 +0000

    Add templates and *.py

---
+++ app/models.py
@@ -0,0 +1,10 @@
+from app import app
+from flask_sqlalchemy import SQLAlchemy
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
---
+++ app/routes.py
@@ -0,0 +1,14 @@
+from app import app
+from flask import render_template
+
+@app.route('/')
+def home():
+    return render_template('index.html.j2')
+
+@app.route('/get-started')
+def getstarted():
+    return render_template('get-started.html.j2')
+
+@app.route('/get-started')
+def howitworks():
+    return render_template('how-it-works.html.j2')
