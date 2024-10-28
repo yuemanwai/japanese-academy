8763d5861c523fa830002735a0c0832415887b3c
Ashley Yue <143982071+yuemanwai@users.noreply.github.com>
2024-10-28 12:34:51 +0000

    update requirements.txt

--- app/routes.py
+++ app/routes.py
@@ -2,17 +2,21 @@ from app import app
 from flask import render_template
 
 @app.route('/')
-def home():
+def index():
     return render_template('index.html.j2')
 
 @app.route('/get-started')
 def getstarted():
     return render_template('get-started.html.j2')
 
-@app.route('/get-started')
+@app.route('/how-it-works')
 def howitworks():
-    return render_template('how-it-works.html.j2')
+    return "需要做 how-it-works.html.j2"
 
 @app.route('/login')
 def login():
-    return "未做好"
+    return "需要做 login.html.j2"
+
+@app.route('/register')
+def login():
+    return "需要做 register.html.j2"
