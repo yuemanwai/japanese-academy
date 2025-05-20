50d1fc41594f3f177f115c63e8d815243ad0b4a0
Ashley Yue <143982071+yuemanwai@users.noreply.github.com>
2025-05-20 09:02:56 +0000

    Setup Allure

---
+++ setup-allure.sh
@@ -0,0 +1,23 @@
+# Install allure command line tool
+sudo apt update
+sudo apt install default-jre
+echo 'export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64' >> ~/.bashrc
+source ~/.bashrc
+wget https://github.com/allure-framework/allure2/releases/download/2.23.0/allure-2.23.0.tgz
+tar -xvzf allure-2.23.0.tgz
+sudo rm allure-2.23.0.tgz
+sudo mv allure-2.23.0 /opt/allure
+sudo ln -s /opt/allure/bin/allure /usr/local/bin/allure
+
+allure --version
+
+
+# 產生 Allure 測試結果
+# pytest --alluredir=allure-results        
+
+# 產生 Allure 報告
+# allure generate allure-results --clean -o allure-report   
+
+# 啟動本地伺服器瀏覽 Allure 報告
+# allure serve allure-results               
+
