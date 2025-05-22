4ddc4eb5a45e47ed9aeda6efaf6c659e9032d9f8
Ashley Yue <143982071+yuemanwai@users.noreply.github.com>
2025-05-22 02:54:24 +0000

    Update CI workflow to ensure build job depends on test job; enhance setup-allure.sh with Java version checks

--- setup-allure.sh
+++ setup-allure.sh
@@ -1,8 +1,14 @@
 # Install allure command line tool
 sudo apt update
 sudo apt install default-jre
+java -version
+sudo update-alternatives --config java
+
 echo 'export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64' >> ~/.bashrc
+echo 'export PATH=$JAVA_HOME/bin:$PATH' >> ~/.bashrc
 source ~/.bashrc
+which java
+
 wget https://github.com/allure-framework/allure2/releases/download/2.23.0/allure-2.23.0.tgz
 tar -xvzf allure-2.23.0.tgz
 sudo rm allure-2.23.0.tgz
@@ -10,6 +16,7 @@ sudo mv allure-2.23.0 /opt/allure
 sudo ln -s /opt/allure/bin/allure /usr/local/bin/allure
 
 allure --version
+
 
 
 # 產生 Allure 測試結果
