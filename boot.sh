6f1c4ae9a4fee45ed8c5c6d347c5d42f4c08ff64
Ashley Yue <143982071+yuemanwai@users.noreply.github.com>
2025-07-21 16:54:34 +0800

    Update boot.sh

--- boot.sh
+++ boot.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 # this script is used to boot a Docker container
-apt-get update
-apt-get install -y xvfb
-
+# apt-get update
+# apt-get install -y xvfb
+apk update && apk add xvfb
 while true; do
     flask db upgrade
     if [[ "$?" == "0" ]]; then
